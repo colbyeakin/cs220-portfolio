a67716fccdc455dbbaebd3b37c7891b3b38ec4a0
colbyeakin <cbe204@ensign.edu>
2025-10-13 22:25:00 +0000

    Undo

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/colbyeakin.github.io",
-  assetPrefix: "/colbyeakin.github.io",
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
+
 };
 
 export default nextConfig;
